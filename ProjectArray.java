ce3694045594f5b05d7a3acb5aa62768f2bede09
fedfed2 <92828828+fedfed2@users.noreply.github.com>
2023-04-10 14:24:39 -0500

    Add files via upload

--- ProjectArray.java
+++ ProjectArray.java
@@ -56,7 +56,7 @@ public class ProjectArray {
     }
 
     public boolean isSorted() {
-        for (int i = 0; i < array.length; i++)
+        for (int i = 0; i < array.length - 1; i++)
             if (array[i] > array[i+1])
                 return false;
         return true;
