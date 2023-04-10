d09d3437b27a8846455da1e6694e20211594cc94
adrianmoore1000 <109485088+adrianmoore1000@users.noreply.github.com>
2023-04-09 21:02:16 -0700

    Add files via upload

--- Sort.java
+++ Sort.java
@@ -12,30 +12,27 @@ public class Sort {
                 max = tmp;
         }
 
+        // compare bits while max element still has bits
         for (int i = 1; max / i > 0; i *= 2) {
-            int ones  = 0;
+            int ones  = size;
             int zeros = 0;
 
-            // count how many elements with radix 0 and 1
+            // count how many elements with radix 0
             for (int j = 0; j < size; j++) {
                 if ((a.get(j) / i) % 2 == 0)
                     zeros++;
-                else 
-                    ones++;
             }
 
-            // ones is now the first index of an element with radix 1
-            ones += zeros;
-
+            // copy elements of radix 0 and 1 separately into tmp array in order
             for (int j = size - 1; j >= 0; j--) {
                 tmp = a.get(j);
-                if ((tmp / i) % 2 == 0) {
+                if ((tmp / i) % 2 == 0)
                     a.setExtra(--zeros,  tmp);
-                } else {
+                else
                     a.setExtra(--ones, tmp);
-                }
             }
 
+            // copy values from tmp array back into array
             for (int j = 0; j < size; j++)
                 a.set(j, a.getExtra(j));
         }
@@ -46,9 +43,8 @@ public class Sort {
         int size = a.length();
 
         // put into heap order
-        for (int i = size / 2 - 1; i >= 0; i--) {
+        for (int i = size / 2 - 1; i >= 0; i--)
             sink(a, i, size);
-        }
         
         // sortdown
         for (int i = size - 1; i > 0; i--) {
@@ -63,9 +59,6 @@ public class Sort {
 
         // loop while parent has children
         while (leftChild < size || rightChild < size) {
-            //System.out.println("Parent is: " + parent);
-            //System.out.println("leftChild is: " + leftChild);
-            //System.out.println("rightChild is: " + rightChild);
             // no left child
             if (leftChild >= size) {
                 if (a.get(rightChild) > a.get(parent))
@@ -87,8 +80,7 @@ public class Sort {
                 parent = leftChild;
             } else {
                 a.swap(rightChild, parent);
-                parent = rightChild;
-        
+                parent = rightChild; 
             }
             leftChild  = 2 * parent + 1;
             rightChild = 2 * parent + 2;
@@ -113,17 +105,19 @@ public class Sort {
         int bottom   = lower;
         int tmpIndex = lower;
         
+        // loop while lower or upper array still has elements
         while (lower <= mid1 || mid2 <= upper) {
-            if (lower > mid1)
+            if (lower > mid1) // lower array is fully copied, so copy rest of upper array
                 a.setExtra(tmpIndex++, a.get(mid2++));
-            else if (mid2 > upper)
+            else if (mid2 > upper) // upper array is fully copied, so copy rest of lower array
                 a.setExtra(tmpIndex++, a.get(lower++));
-            else if (a.get(lower) <= a.get(mid2))
+            else if (a.get(lower) <= a.get(mid2)) // copy the smaller element among the two arrays
                 a.setExtra(tmpIndex++, a.get(lower++));
             else
                 a.setExtra(tmpIndex++, a.get(mid2++));
         }
 
+        // copy values from tmp array back into array
         for (int i = bottom; i <= upper; i++)
             a.set(i, a.getExtra(i));
     }
